4d76ff25139833ff4732935312cffb98a2d80765
Latif siddiq sunny <lsiddiqsunny@gmail.com>
2017-12-16 01:18:27 +0600

    Update publisher's GUI

---
+++ src/Main/bookinfo.java
@@ -0,0 +1,32 @@
+package Main;
+
+public  class bookinfo {
+    private final String Bookname;
+    private final String Authorname;
+    private final String Price;
+
+    public bookinfo(String fName, String lName, String email) {
+        this.Bookname = new String(fName);
+        this.Authorname = new String(lName);
+        this.Price = new String(email);
+    }
+
+    public String getBookname() {
+        return Bookname;
+    }
+
+
+    public String getAuthorname() {
+        return Authorname;
+    }
+
+
+    public String getPrice() {
+        return Price;
+    }
+
+    public String toString() {
+       return Bookname+" - "+Authorname+" - "+Price;
+    }
+
+}
